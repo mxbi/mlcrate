dd8385065504710cd67ea74ce05d5c3dd04f580c
Mikel Bober-Irizar <mikel@mxbi.net>
2017-12-26 13:51:59 +0100

    Add LinewiseCSVWriter
    Also add docstring to TimeTracker

--- mlcrate/__init__.py
+++ mlcrate/__init__.py
@@ -1,5 +1,8 @@
 import pickle
 import gzip
+import os
+
+from . import time
 
 __version__ = '0.0.0a1'
 
@@ -44,3 +47,45 @@ def save_kaggle_sub(df, filename='sub.csv.gz'):
         compression = None
 
     df.to_csv(filename, index=False, compression=compression)
+
+class LinewiseCSVWriter:
+    def __init__(self, filename, header=None, sync=True, append=False):
+        """CSV Writer which writes a single line at a time, and by default syncs to disk after every line.
+        This is useful for eg. log files, where you want progress to appear in the file as it happens (instead of being written to disk when python exists)
+        Data should be passed to the writer as an iterable, as conversion to string and so on is done within the class.
+
+        Keyword arguments:
+        filename -- the csv file to write to
+        header (default: None) -- An iterator (eg. list) containing an optional CSV header, which is written as the first line of the file.
+        sync (default: True) -- Flush and sync the output to disk after every write operation. This means data appears in the file instantly instead of being buffered
+        append (default: False) -- Append to an existing CSV file. By default, the csv file is overwritten each time.
+        """
+        self.autoflush = sync
+        self.len = None
+
+        mode = 'a' if append else 'w'
+        self.f = open(filename, mode)
+
+        if header:
+            self.write(header)
+
+    def __del__(self):
+        self.flush()
+        self.f.close()
+
+    def write(self, data):
+        """Write a line of data to the csv file - data should be an iterable."""
+        if self.len is None:
+            self.len = len(data)
+        assert len(data) == self.len, 'Length of passed data does not match previous rows'
+
+        text = '"' + '","'.join([str(x).replace('"', '""') for x in data]) + '"\n'
+
+        self.f.write(text)
+        if self.autoflush:
+            self.flush()
+
+    def flush(self):
+        """Manually flush the csv. Useless if flush=True in the class, as this is called after every write anyway."""
+        self.f.flush()
+        os.fsync(self.f.fileno())
--- mlcrate/time.py
+++ mlcrate/time.py
@@ -2,6 +2,15 @@ import time
 import datetime
 
 class TimeTracker:
+    """A class for tracking timestamps and time elapsed since events. Useful for profiling code.
+
+    Usage:
+    >>> t = TimeTracker()
+    >>> t.elapsed(0) # Seconds since the timetracker was initialised
+    >>> t.add('func') # Save the current timestamp as 'func'
+    >>> t.elapsed('func') # Seconds since 'func' was added
+    >>> t['func'] # Get the absolute timestamp of 'func' for other uses
+    """
     def __init__(self):
         self.times = {}
         self.add(0)
@@ -10,11 +19,13 @@ class TimeTracker:
         return self.times[key]
 
     def add(self, key):
+        """Add the current time to the index with the specified key"""
         self.times[key] = time.time()
 
     def elapsed(self, key):
+        """Get the time passed in seconds since the specified key was added to the index"""
         return time.time() - self.times[key]
 
-def get_current_str_time():
+def str_time_now():
     """Returns the current time as a string in the format 'YYYY_MM_DD_HH_MM_SS'. Useful for timestamping filenames etc."""
     return time.strftime("%Y_%m_%d_%H_%M_%S")
