5dcbd191670202e715ccee6180104d7cc9d71faf
Mikel Bober-Irizar <mikel.cat7@gmail.com>
2018-07-20 17:51:06 -0400

    Update torch.totensor and add torch.MLP

--- mlcrate/torch.py
+++ mlcrate/torch.py
@@ -22,14 +22,23 @@ def tonp(tensor):
 def totensor(arr, device=None, type='float32'):
     """Converts any array-like or scalar to a PyTorch tensor, and checks that the array is in the correct type (defaults to float32) and on the correct device.
     Equivalent to calling `torch.from_array(np.array(arr, dtype=type)).to(device)` but more efficient.
+    NOTE: If the input is a torch tensor, the type will not be checked.
 
     Keyword arguments:
     arr -- Any array-like object (eg numpy array, list, numpy varaible)
-    features -- a list of feature names corresponding to the features the model was trained on.
+    device (optional) -- Move the tensor to this device after creation
+    type -- the numpy data type of the tensor. Defaults to 'float32' (regardless of the input)
 
     Returns:
-    importance -- A list of (feature, importance) tuples representing sorted importance"""
+    tensor - A torch tensor"""
     _check_torch_import()
+    # If we're given a tensor, send it right back.
+    if isinstance(arr, torch.Tensor):
+        if device:
+            return arr.to(device) # If tensor is already on the specified device, this doesn't copy the tensor.
+        else:
+            return arr
+
     # Only call np.array() if it is not already an array, otherwise numpy will waste time copying the array
     if not isinstance(arr, np.ndarray):
         arr = np.array(arr)
@@ -41,3 +50,41 @@ def totensor(arr, device=None, type='float32'):
     if device:
         tensor = tensor.to(device)
     return tensor
+
+# We define the class only at call-time to allow for lazy torch import
+# TODO: Find a less hacky way of doing this. If you have any ideas let me know :)
+def MLP(dim, dropout=0.3, hidden_activation=torch.nn.ReLU, output_activation=None):
+    """A fully connected MLP network with the specified sizes at each layer.
+
+    Arguments:
+    shape -- A list of the shape of the network. The first item is the input size and the last item is the output size.
+             Eg. [128, 512, 512, 1] defines a network with 128 inputs, 1 output and two hidden layers of size 512.
+    dropout (default: 0.3) -- Proportion for dropout inbetween layers
+    hidden_activation (default: torch.nn.ReLU) -- Activation function between layers.
+    output_activation (optional) -- Activation function at the end of the network.
+    """
+    _check_torch_import()
+
+    class mlc_MLP(torch.nn.Module):
+        def __init__(self, dim, dropout, hidden_activation, output_activation):
+            super(self, mlc_MLP).__init__()
+
+            self.layers = []
+            for f_in, f_out in zip(dim[:-2], dim[1:-1]):
+                self.layers += [
+                    torch.nn.Linear(f_in, f_out),
+                    hidden_activation(),
+                    torch.nn.Dropout(dropout),
+                ]
+
+            self.layers += [torch.nn.Linear(dim[-2], dim[-1])]
+
+            if output_activation:
+                self.layers += [output_activation()]
+
+            self.model = torch.nn.Sequential(*self.layers)
+
+        def forward(self, x):
+            return self.model(x)
+
+    return mlc_MLP(dim, dropout, hidden_activation, output_activation)
